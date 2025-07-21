47e0871f76ad6e7bfded81bd42606cd78cb5b6dc
E <emilakbarov857@gmail.com>
2025-07-21 23:56:19 +0400

    adjusted routing

--- DjangoChat/asgi.py
+++ DjangoChat/asgi.py
@@ -1,16 +1,19 @@
-"""
-ASGI config for DjangoChat project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
 
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter,URLRouter
+import chatapp.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoChat.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chatapp.routing.websocket_urlpatterns
+        )
+    )
+})
